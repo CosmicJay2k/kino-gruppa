16505dfd9d59169656d9608f8551ea7d4332a354
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2022-02-05 10:50:35 +0100

    Change date format

--- public/js/modules/renderSingleScreenings.js
+++ public/js/modules/renderSingleScreenings.js
@@ -4,6 +4,13 @@ export async function renderSingleScreenings(id) {
   const screenings = await fetchSingleScreenings(id);
   const container = document.querySelector(".right-container-content-calendar");
 
+  const dateOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+
   screenings.forEach((screening) => {
     const newScreening = document.createElement("div");
 
@@ -11,9 +18,14 @@ export async function renderSingleScreenings(id) {
     const dateTimeArr = screening.attributes.start_time.split("T");
     const timeArr = dateTimeArr[1].split(":");
 
+    const date = new Date(dateTimeArr[0]).toLocaleDateString(
+      "se-SE",
+      dateOptions
+    );
+
     const newTime = document.createElement("h3");
     const newTimeNode = document.createTextNode(
-      dateTimeArr[0] + ", " + timeArr[0] + ":" + timeArr[1]
+      date + ". " + timeArr[0] + ":" + timeArr[1]
     );
     newTime.appendChild(newTimeNode);
 
